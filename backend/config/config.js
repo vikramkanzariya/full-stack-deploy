9e9f393329d3a2a77e1e6a32e7cb842679381b89
vikramkanzariya <vikramdalvadi@gmail.com>
2026-05-11 10:16:39 +0530

    backend changes

---
+++ backend/config/config.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+    logging: false
+  },
+
+  test: {
+    username: process.env.TEST_DB_USER,
+    password: process.env.TEST_DB_PASSWORD,
+    database: process.env.TEST_DB_NAME,
+    host: process.env.TEST_DB_HOST,
+    dialect: "mysql"
+  },
+
+  production: {
+    username: process.env.PROD_DB_USER,
+    password: process.env.PROD_DB_PASSWORD,
+    database: process.env.PROD_DB_NAME,
+    host: process.env.PROD_DB_HOST,
+    dialect: "mysql"
+  }
+};
